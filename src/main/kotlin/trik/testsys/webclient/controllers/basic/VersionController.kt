b323216f4f46f3921e3b00779e9e80433d8bd04c
5h15h4k1n9 <st077017@studetn.spbu.ru>
2023-04-13 12:48:18 +0300

    Updated version controller

--- src/main/kotlin/trik/testsys/webclient/controllers/basic/VersionController.kt
+++ src/main/kotlin/trik/testsys/webclient/controllers/basic/VersionController.kt
@@ -1,28 +1,33 @@
 package trik.testsys.webclient.controllers.basic
 
-import com.beust.klaxon.JsonObject
+import io.swagger.annotations.ApiOperation
+import io.swagger.annotations.ApiResponse
+
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
+import trik.testsys.webclient.swagger.models.basic.AppVersion
+
 @RestController
-class VersionController(@Value("\${app.version}") final val appVersion: String) {
+class VersionController(@Value("\${app.version}") val appVersion: String) {
 
     private val logger: Logger = LoggerFactory.getLogger(this::class.java)
 
-    private val appVersionJson = JsonObject(
-        mapOf(
-            "version" to appVersion
-        )
+    @ApiOperation(
+        value = "Get info about service version.",
+        notes = "Returns service version.",
+        response = AppVersion::class,
     )
-
-    @GetMapping("/version")
-    fun getVersion(): ResponseEntity<JsonObject> {
+    @ApiResponse(code = 200, message = "Json with service version.")
+    @GetMapping("/version", produces = [MediaType.APPLICATION_JSON_VALUE])
+    fun getVersion(): ResponseEntity<AppVersion> {
         logger.info("Client requested application version.")
-        return ResponseEntity.ok().body(appVersionJson)
+        return ResponseEntity.ok().body(AppVersion(appVersion))
     }
 }
