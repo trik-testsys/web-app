99d4b205b792945c007cff9a466d10cea67e9afb
5h15h4k1n9 <st077017@studetn.spbu.ru>
2023-04-13 12:47:28 +0300

    Updated health controller

--- src/main/kotlin/trik/testsys/webclient/controllers/basic/HealthController.kt
+++ src/main/kotlin/trik/testsys/webclient/controllers/basic/HealthController.kt
@@ -1,7 +1,12 @@
 package trik.testsys.webclient.controllers.basic
 
+import io.swagger.annotations.Api
+import io.swagger.annotations.ApiOperation
+import io.swagger.annotations.ApiResponse
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import org.springframework.http.MediaType
+
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
@@ -11,8 +16,13 @@ class HealthController {
 
     private val logger: Logger = LoggerFactory.getLogger(this::class.java)
 
+    @ApiOperation(
+        value = "Get info about service health.",
+        notes = "Returns service health.",
+    )
+    @ApiResponse(code = 200, message = "OK if service is alive.")
     @GetMapping("/health")
-    fun getHealth(): ResponseEntity<Any> {
+    fun getHealth(): ResponseEntity<Void> {
         logger.info("Client requested health info.")
         return ResponseEntity.ok().build()
     }
