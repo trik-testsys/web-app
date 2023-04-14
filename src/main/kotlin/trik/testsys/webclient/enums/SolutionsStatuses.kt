99f3281adbeefdf5e9464d90bcc7debf45ca3dcc
5h15h4k1n9 <st077017@studetn.spbu.ru>
2023-04-14 18:44:14 +0300

    Added repository and service

---
+++ src/main/kotlin/trik/testsys/webclient/enums/SolutionsStatuses.kt
@@ -0,0 +1,9 @@
+package trik.testsys.webclient.enums
+
+enum class SolutionsStatuses {
+    FAILED,
+    PASSED,
+    IN_PROGRESS,
+    NOT_STARTED,
+    ERROR
+}
---
+++ src/main/kotlin/trik/testsys/webclient/services/SolutionService.kt
@@ -0,0 +1,42 @@
+package trik.testsys.webclient.services
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Service
+import trik.testsys.webclient.entities.Solution
+
+import trik.testsys.webclient.repositories.SolutionRepository
+import trik.testsys.webclient.repositories.StudentRepository
+import trik.testsys.webclient.repositories.TaskRepository
+
+
+@Service
+class SolutionService {
+
+    @Autowired
+    lateinit var solutionRepository: SolutionRepository
+
+    @Autowired
+    lateinit var studentRepository: StudentRepository
+
+    @Autowired
+    lateinit var taskRepository: TaskRepository
+
+    fun getAllStudentSolutionsByTask(studentId: Long, taskId: Long): List<Solution>? {
+        val student = studentRepository.findStudentById(studentId) ?: return null
+        val task = taskRepository.findTaskById(taskId) ?: return null
+
+        return solutionRepository.findSolutionsByStudentAndTask(student, task)
+    }
+
+    fun getAllStudentSolutions(studentId: Long): List<Solution>? {
+        val student = studentRepository.findStudentById(studentId) ?: return null
+
+        return solutionRepository.findSolutionsByStudent(student)
+    }
+
+    fun getAllTaskSolutions(taskId: Long): List<Solution>? {
+        val task = taskRepository.findTaskById(taskId) ?: return null
+
+        return solutionRepository.findSolutionsByTask(task)
+    }
+}
