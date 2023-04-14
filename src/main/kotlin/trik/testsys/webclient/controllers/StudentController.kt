c399190f662e0c984985b633c02adedef85c2b40
5h15h4k1n9 <st077017@studetn.spbu.ru>
2023-04-14 16:47:37 +0300

    Added student page

--- src/main/kotlin/trik/testsys/webclient/controllers/StudentController.kt
+++ src/main/kotlin/trik/testsys/webclient/controllers/StudentController.kt
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import trik.testsys.webclient.enums.WebUserStatuses
 import trik.testsys.webclient.models.ResponseMessage
 
 import trik.testsys.webclient.services.GroupService
@@ -78,4 +79,38 @@ class StudentController {
         return model
     }
 
+    @GetMapping
+    fun getAccess(@RequestParam accessToken: String, model: Model): Any {
+        logger.info("[${accessToken.padStart(80)}]: Client trying to access student page.")
+
+        val status = validateStudent(accessToken)
+        if (status == WebUserStatuses.NOT_FOUND || status == WebUserStatuses.ADMIN) {
+            logger.info("[${accessToken.padStart(80)}]: Client is not a student.")
+            return ResponseEntity
+                .status(HttpStatus.FORBIDDEN)
+                .body(ResponseMessage(403, "You are not a student!"))
+        }
+
+        logger.info("[${accessToken.padStart(80)}]: Client is a student.")
+        val webUser = webUserService.getWebUserByAccessToken(accessToken)!!
+        val student = studentService.getStudentByWebUser(webUser)!!
+
+        model.addAttribute("id", student.id)
+        model.addAttribute("username", webUser.username)
+        model.addAttribute("accessToken", accessToken)
+        model.addAttribute("groupName", student.group.name)
+        return model
+    }
+
+    private fun validateStudent(accessToken: String): WebUserStatuses {
+        val webUser = webUserService.getWebUserByAccessToken(accessToken) ?: run {
+            return WebUserStatuses.NOT_FOUND
+        }
+
+        val student = studentService.getStudentByWebUser(webUser) ?: run {
+            return WebUserStatuses.ADMIN
+        }
+
+        return WebUserStatuses.WEB_USER
+    }
 }
