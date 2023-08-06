66de97d16067c97beed63aa43013cac1873870ed
Roman Shishkin <st077017@student.spbu.ru>
2023-08-06 18:53:38 +0300

    Updated configuration for adding static files

--- src/main/kotlin/trik/testsys/webclient/config/Configuration.kt
+++ src/main/kotlin/trik/testsys/webclient/config/Configuration.kt
@@ -8,11 +8,26 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
+
 
 @Configuration
 @EnableWebSecurity
 @EnableScheduling
-class Configuration {
+class Configuration : WebMvcConfigurerAdapter() {
+
+    override fun addResourceHandlers(registry: ResourceHandlerRegistry) {
+        registry.addResourceHandler(
+            "/img/**",
+            "/css/**",
+            "/js/**"
+        ).addResourceLocations(
+            "classpath:/static/img/",
+            "classpath:/static/css/",
+            "classpath:/static/js/"
+        )
+    }
 
     @Bean
     @Throws(Exception::class)
