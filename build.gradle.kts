2b7430d1642a7d43b5cfd05414d158aad5e028e1
Roman Shishkin <st077017@student.spbu.ru>
2023-10-09 04:53:02 +0300

    Updated gradle settings

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 
 group = "Pupsen&Vupsen"
 version = "1.1.0"
-java.sourceCompatibility = JavaVersion.VERSION_16
+java.sourceCompatibility = JavaVersion.VERSION_17
 
 repositories {
     mavenCentral()
