703dcf3d0fb2614c2ae147e1a4ad1b2cfdcad4ef
Roman Shishkin <st077017@student.spbu.ru>
2023-10-12 01:26:20 +0300

    Refactored developer page

--- src/main/resources/static/js/popup.js
+++ src/main/resources/static/js/popup.js
@@ -1,20 +1,41 @@
-//region Scripts for change info popup
-function showChangeInfoPopup() {
-    var changeInfoPopup = document.getElementById("changeInfoPopup");
-    changeInfoPopup.classList.add("show");
+function showPopup(popupId) {
+    var popup = document.getElementById(popupId);
+    popup.classList.add("show");
 }
 
-function closeChangeInfoPopup() {
-    var changeInfoPopup = document.getElementById("changeInfoPopup");
-    changeInfoPopup.classList.remove("show");
+function closePopup(popupId) {
+    var popup = document.getElementById(popupId);
+    popup.classList.remove("show");
 
-    var form = changeInfoPopup.querySelector("form");
+    var form = popup.querySelector("form");
     form.reset();
 }
 
-closeChangeInfo.addEventListener("click", function () {
-    var changeInfoPopup = document.getElementById("changeInfoPopup");
-    changeInfoPopup.classList.remove("show");
-});
+function addFileInput(id) {
+    const fileInputs = document.getElementById(id);
+    const newInputDiv = document.createElement('div');
+    const newInput = document.createElement('input');
+    newInput.type = 'file';
+    newInput.required = true;
+    newInput.name = 'tests';
+    newInput.accept = 'text/xml';
+    const removeButton = document.createElement('button');
+    removeButton.type = 'button';
+    removeButton.textContent = 'Убрать';
+    removeButton.addEventListener('click', function() {
+        fileInputs.removeChild(newInputDiv);
+    });
+    newInputDiv.appendChild(newInput);
+    newInputDiv.appendChild(removeButton);
+    fileInputs.appendChild(newInputDiv);
+}
 
-//endregion
+function removeFileInput(inputDiv) {
+    const fileInputs = document.getElementById(id);
+    fileInputs.removeChild(inputDiv.parentElement);
+}
+
+function clearInputFile(id) {
+    const fileInputs = document.getElementById(id);
+    fileInputs.innerHTML = '';
+}
