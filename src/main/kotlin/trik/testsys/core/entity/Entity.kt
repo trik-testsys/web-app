ae4f715269216f21c8bd541f789e5f0122c199b0
Roman Shishkin <st077017@student.spbu.ru>
2024-06-17 07:51:16 +0300

    TWC-2 Identify core abstractions in the system
    Refactored entities core

---
+++ src/main/kotlin/trik/testsys/core/entity/Entity.kt
@@ -0,0 +1,22 @@
+package trik.testsys.core.entity
+
+import org.springframework.data.domain.Persistable
+import java.time.LocalDateTime
+
+/**
+ * Simple interface for entities. It inherits [getId] (val property id) and [isNew] (val property isNew) from [Persistable],
+ * and extends it with [creationDate] field.
+ *
+ * @author Roman Shishkin
+ * @since 2.0.0
+ */
+interface Entity : Persistable<Long> {
+
+    /**
+     * Property which says date and time of entity creation.
+     *
+     * @author Roman Shishkin
+     * @since 2.0.0
+     */
+    val creationDate: LocalDateTime
+}
