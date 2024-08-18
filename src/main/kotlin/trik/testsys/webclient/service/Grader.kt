41422f12a4a8c30fb995f0b05d44e880c81989f9
Roman Shishkin <st077017@student.spbu.ru>
2024-08-18 22:45:51 +0500

    TWC-9 Design balancer class
    Added Grader interface

---
+++ src/main/kotlin/trik/testsys/webclient/service/Grader.kt
@@ -0,0 +1,32 @@
+package trik.testsys.webclient.service
+
+import trik.testsys.webclient.entity.Solution
+import trik.testsys.webclient.entity.Task
+
+interface Grader {
+
+    fun sendToGrade(solution: Solution, task: Task, gradingOptions: GradingOptions)
+
+    fun subscribeOnGraded(onGraded: (GradingInfo) -> Unit)
+
+    fun addNode(address: String)
+
+    fun removeNode(address: String)
+
+    fun getNodeStatus(address: String): NodeStatus
+
+    fun getAllNodeStatuses(): Map<String, NodeStatus>
+
+    data class GradingInfo(
+        val solutionId: Long,
+    )
+
+    data class NodeStatus(
+        val isAlive: Boolean,
+    )
+
+    data class GradingOptions(
+        val shouldRecord: Boolean,
+        val trikStudioVersion: String
+    )
+}
