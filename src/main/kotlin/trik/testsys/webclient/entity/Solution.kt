762ef86be30d3bdcc91519f18d631dbdc7fce5ad
Roman Shishkin <st077017@student.spbu.ru>
2024-08-18 22:52:58 +0500

    TWC-9 Design balancer class
    Added fields

--- src/main/kotlin/trik/testsys/webclient/entity/Solution.kt
+++ src/main/kotlin/trik/testsys/webclient/entity/Solution.kt
@@ -1,49 +1,8 @@
 package trik.testsys.webclient.entity
 
-import java.util.Date
-import javax.persistence.*
+interface Solution {
 
-@Entity
-@Table(name = "SOLUTIONS")
-class Solution(
-    @ManyToOne
-    @JoinColumn(name = "student_id", nullable = false)
-    val student: Student,
+    val id: Long
 
-    @ManyToOne
-    @JoinColumn(name = "task_id", nullable = false)
-    val task: Task,
-
-    @Column(nullable = false, unique = true)
-    val gradingId: Long
-) {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(nullable = false, unique = true)
-    val id: Long? = null
-
-    @Column(nullable = false, columnDefinition = "BIGINT DEFAULT 0")
-    var countOfTests: Long = 0L
-
-    @Column(nullable = false, columnDefinition = "BIGINT DEFAULT 0")
-    var countOfPassedTests: Long = 0L
-
-    @Column(nullable = false, columnDefinition = "BIGINT DEFAULT 3")
-    var status: Status = Status.NOT_STARTED
-
-    @Column(nullable = false)
-    var date: Date = Date()
-
-    /**
-     * @author Roman Shishkin
-     * @since 1.1.0
-     */
-    enum class Status {
-        FAILED,
-        PASSED,
-        IN_PROGRESS,
-        NOT_STARTED,
-        ERROR
-    }
+    val name: String
 }
