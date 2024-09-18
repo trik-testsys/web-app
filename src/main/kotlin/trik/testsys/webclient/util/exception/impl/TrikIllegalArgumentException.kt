60de908e80e9edad98632d4672d1d8eb23da6b50
Roman Shishkin <st077017@student.spbu.ru>
2024-09-18 04:37:01 +0300

    TWC-3 Fix hierarchy
    Removed useless classes

--- src/main/kotlin/trik/testsys/webclient/util/exception/impl/TrikIllegalArgumentException.kt
+++
@@ -1,8 +0,0 @@
-package trik.testsys.webclient.util.exception.impl
-
-import trik.testsys.webclient.util.exception.TrikException
-
-/**
- * @since 1.1.0.14-alpha
- */
-class TrikIllegalArgumentException(message: String) : IllegalArgumentException(message), TrikException
--- src/main/kotlin/trik/testsys/webclient/util/exception/impl/TrikIllegalStateException.kt
+++
@@ -1,9 +0,0 @@
-package trik.testsys.webclient.util.exception.impl
-
-import trik.testsys.webclient.util.exception.TrikException
-
-/**
- * @author Roman Shishkin
- * @since 1.1.0
- */
-class TrikIllegalStateException(message: String) : IllegalStateException(message), TrikException
