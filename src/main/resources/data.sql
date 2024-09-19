ccff12aad0b80429ae139c56f5d9dee79f8b407a
Roman Shishkin <st077017@student.spbu.ru>
2024-09-19 03:02:12 +0300

    TWC-3 Fix hierarchy
    Removed useless files

--- src/main/resources/data.sql
+++
@@ -1,7 +0,0 @@
-
-
-INSERT INTO web_users(access_token, username)
-VALUES ('superuser', 'Super Puper User');
-
-INSERT INTO super_users(web_user_id)
-VALUES ((SELECT web_users.id FROM web_users WHERE web_users.access_token = 'superuser'))
