1305db415b6470a0f6fd2fa62f34bd13dac450bf
Roman Shishkin <st077017@student.spbu.ru>
2024-09-24 04:06:59 +0300

    TWC-3 Fix hierarchy
    Added StudentView

---
+++ src/main/kotlin/trik/testsys/webclient/view/StudentView.kt
@@ -0,0 +1,43 @@
+package trik.testsys.webclient.view
+
+import trik.testsys.core.entity.AbstractEntity
+import trik.testsys.core.entity.user.AccessToken
+import trik.testsys.core.view.user.UserView
+import trik.testsys.webclient.entity.impl.Group
+import trik.testsys.webclient.entity.user.impl.Student
+import trik.testsys.webclient.util.atTimeZone
+import trik.testsys.webclient.util.fromTimeZone
+import java.time.LocalDateTime
+import java.util.TimeZone
+import javax.xml.bind.annotation.XmlRootElement
+
+@XmlRootElement
+data class StudentView(
+    override val id: Long?,
+    override val name: String,
+    override val accessToken: AccessToken,
+    val creationDate: LocalDateTime?,
+    val lastLoginDate: LocalDateTime,
+    val group: Group
+) : UserView<Student> {
+
+
+    override fun toEntity(timeZone: TimeZone) = Student(
+        name, accessToken, group
+    ).also {
+        it.id = id
+        it.lastLoginDate = lastLoginDate.fromTimeZone(timeZone)
+    }
+
+    companion object {
+
+        fun Student.toView(timeZone: TimeZone) = StudentView(
+            id = this.id,
+            name = this.name,
+            accessToken = this.accessToken,
+            creationDate = this.creationDate?.atTimeZone(timeZone),
+            lastLoginDate = this.lastLoginDate.atTimeZone(timeZone),
+            group = this.group
+        )
+    }
+}
