0179141ff4ff8dc1b49d900b8e56ecb95bcb1b55
Roman Shishkin <st077017@student.spbu.ru>
2024-09-24 04:08:13 +0300

    TWC-3 Fix hierarchy
    Added user controller interface

---
+++ src/main/kotlin/trik/testsys/webclient/controller/user/UserController.kt
@@ -0,0 +1,9 @@
+package trik.testsys.webclient.controller.user
+
+import trik.testsys.core.utils.marker.TrikController
+
+/**
+ * @author Roman Shishkin
+ * @since 2.0.0
+**/
+interface UserController : TrikController
