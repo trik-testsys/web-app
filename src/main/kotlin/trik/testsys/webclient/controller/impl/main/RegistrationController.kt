004945ecd82aeab34af0c9ab49f2756b38197fec
Roman Shishkin <st077017@student.spbu.ru>
2024-09-29 04:27:17 +0300

    TWC-3 Fix hierarchy
    Updated registration controller entity

--- src/main/kotlin/trik/testsys/webclient/controller/impl/main/RegistrationController.kt
+++ src/main/kotlin/trik/testsys/webclient/controller/impl/main/RegistrationController.kt
@@ -1,6 +1,6 @@
 package trik.testsys.webclient.controller.impl.main
 
-import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.context.ApplicationContext
 import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
@@ -10,16 +10,9 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.servlet.View
 import org.springframework.web.servlet.mvc.support.RedirectAttributes
-import trik.testsys.core.entity.user.AccessToken
 import trik.testsys.core.entity.user.UserEntity
-import trik.testsys.webclient.entity.user.impl.Admin
-import trik.testsys.webclient.entity.user.impl.Student
-import trik.testsys.webclient.service.entity.impl.GroupService
-import trik.testsys.webclient.service.entity.user.impl.AdminService
-import trik.testsys.webclient.service.entity.user.impl.StudentService
-import trik.testsys.webclient.service.entity.user.impl.ViewerService
+import trik.testsys.webclient.service.entity.RegEntityService
 import trik.testsys.webclient.service.security.login.impl.LoginData
-import trik.testsys.webclient.service.token.access.AccessTokenGenerator
 import trik.testsys.webclient.util.addPopupMessage
 import trik.testsys.webclient.util.addSessionActiveInfo
 import javax.servlet.http.HttpServletRequest
@@ -31,16 +24,11 @@ import javax.servlet.http.HttpServletRequest
 @Controller
 @RequestMapping(RegistrationController.REGISTRATION_PATH)
 class RegistrationController(
-    private val loginData: LoginData,
-
-    private val studentService: StudentService,
-    private val adminService: AdminService,
-    private val groupService: GroupService,
-    private val viewerService: ViewerService,
-
-    @Qualifier("studentAccessTokenGenerator") private val studentAccessTokenGenerator: AccessTokenGenerator,
-    @Qualifier("webUserAccessTokenGenerator") private val webUserAccessTokenGenerator: AccessTokenGenerator
+    context: ApplicationContext,
+    private val loginData: LoginData
 ) {
+
+    private val registrationServices = context.getBeansOfType(RegEntityService::class.java).values
 
     @GetMapping
     fun registrationGet(model: Model): String {
@@ -55,37 +43,19 @@ class RegistrationController(
         redirectAttributes: RedirectAttributes,
         request: HttpServletRequest
     ): String {
-        if (name.contains(regToken)) {
+        val neededService = registrationServices.firstOrNull {
+            it.findByRegToken(regToken) != null
+        } ?: run {
+            redirectAttributes.addPopupMessage("Некорректный Код-доступа. Попробуйте еще раз.")
+            return "redirect:$REGISTRATION_PATH"
+        }
+
+        val registeredEntity = neededService.register(regToken, name) { !it.name.contains(regToken) } ?: run {
             redirectAttributes.addPopupMessage("Псевдоним не должен содержать Код-регистрации. Попробуйте другой вариант.")
             return "redirect:$REGISTRATION_PATH"
         }
 
-        groupService.findByAccessToken(regToken)?.let {
-            val accessToken = studentAccessTokenGenerator.generate(name)
-            val student = Student(name, accessToken, it)
-            studentService.save(student)
-
-            return getLoginRedirection(accessToken, redirectAttributes, request)
-        }
-
-        viewerService.findByAdminRegToken(regToken)?.let {
-            val accessToken = webUserAccessTokenGenerator.generate(name)
-            val admin = Admin(name, accessToken, it)
-            adminService.save(admin)
-
-            return getLoginRedirection(accessToken, redirectAttributes, request)
-        }
-
-        redirectAttributes.addPopupMessage("Некорректный Код-доступа. Попробуйте еще раз.")
-        return "redirect:$REGISTRATION_PATH"
-    }
-
-    private fun getLoginRedirection(
-        accessToken: AccessToken,
-        redirectAttributes: RedirectAttributes,
-        request: HttpServletRequest
-    ): String {
-        redirectAttributes.addAttribute(UserEntity::accessToken.name, accessToken)
+        redirectAttributes.addAttribute(UserEntity::accessToken.name, registeredEntity.accessToken)
         request.setAttribute(View.RESPONSE_STATUS_ATTRIBUTE, HttpStatus.TEMPORARY_REDIRECT)
 
         return "redirect:${LoginController.LOGIN_PATH}"
