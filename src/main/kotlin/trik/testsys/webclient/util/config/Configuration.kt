b8f37b08354bea9748bd022b48977edb4be36f9c
Roman Shishkin <st077017@student.spbu.ru>
2024-09-29 03:11:09 +0300

    TWC-3 Fix hierarchy
    Moved configuration class

--- src/main/kotlin/trik/testsys/webclient/util/config/Configuration.kt
+++ src/main/kotlin/trik/testsys/webclient/util/config/Configuration.kt
@@ -1,4 +1,4 @@
-package trik.testsys.webclient.config
+package trik.testsys.webclient.util.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
