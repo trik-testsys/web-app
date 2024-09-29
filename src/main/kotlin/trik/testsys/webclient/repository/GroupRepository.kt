876d075686970effc8917991df3efe51b09a56c5
Roman Shishkin <st077017@student.spbu.ru>
2024-09-29 03:33:26 +0300

    TWC-3 Fix hierarchy
    Created `RegEntityRepository`

--- src/main/kotlin/trik/testsys/webclient/repository/GroupRepository.kt
+++ src/main/kotlin/trik/testsys/webclient/repository/GroupRepository.kt
@@ -1,14 +1,11 @@
 package trik.testsys.webclient.repository
 
 import org.springframework.stereotype.Repository
-import trik.testsys.core.repository.EntityRepository
 import trik.testsys.webclient.entity.user.impl.Admin
 import trik.testsys.webclient.entity.impl.Group
 
 @Repository
-interface GroupRepository: EntityRepository<Group> {
+interface GroupRepository : RegEntityRepository<Group> {
 
     fun findGroupsByAdmin(admin: Admin): List<Group>?
-
-    fun findByAccessToken(accessToken: String): Group?
 }
