53ec926978f7425e797d3424f7308247cc74bd68
Roman Shishkin <st077017@student.spbu.ru>
2024-10-09 04:23:42 +0300

    TWC-3 Fix hierarchy
    Added abstract main web user controller

---
+++ src/main/kotlin/trik/testsys/webclient/controller/user/AbstractWebUserController.kt
@@ -0,0 +1,60 @@
+package trik.testsys.webclient.controller.user
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.servlet.mvc.support.RedirectAttributes
+import trik.testsys.core.repository.user.UserRepository
+import trik.testsys.core.view.user.UserView
+import trik.testsys.webclient.entity.user.WebUser
+import trik.testsys.webclient.service.entity.user.WebUserService
+import trik.testsys.webclient.service.security.login.impl.LoginData
+import trik.testsys.webclient.util.addExitMessage
+import trik.testsys.webclient.util.addSessionExpiredMessage
+import java.util.TimeZone
+
+/**
+ * @author Roman Shishkin
+ * @since 2.0.0
+ **/
+abstract class AbstractWebUserController<U : WebUser, V : UserView<U>, S : WebUserService<U, out UserRepository<U>>>(
+    protected val loginData: LoginData
+) {
+
+    protected abstract val mainPath: String
+
+    protected abstract val mainPage: String
+
+    @Autowired
+    protected lateinit var service: S
+
+    protected abstract fun U.toView(timeZone: TimeZone): V
+
+    /**
+     * @author Roman Shishkin
+     * @since 2.0.0
+     **/
+    protected fun LoginData.validate(redirectAttributes: RedirectAttributes): U? {
+        val webUser = accessToken?.let { service.findByAccessToken(it) } ?: run {
+            invalidate()
+            redirectAttributes.addSessionExpiredMessage()
+            return null
+        }
+
+        return webUser
+    }
+
+    /**
+     * @author Roman Shishkin
+     * @since 2.0.0
+     **/
+    protected fun String?.checkLogout(redirectAttributes: RedirectAttributes): Boolean {
+        this ?: return false
+
+        redirectAttributes.addExitMessage()
+        return true
+    }
+
+    companion object {
+
+        const val WEB_USER_ATTR = "webUser"
+    }
+}
--- src/main/kotlin/trik/testsys/webclient/controller/user/AbstractWebUserMainController.kt
+++ src/main/kotlin/trik/testsys/webclient/controller/user/AbstractWebUserMainController.kt
@@ -1,6 +1,5 @@
 package trik.testsys.webclient.controller.user
 
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.ModelAttribute
@@ -14,25 +13,12 @@ import trik.testsys.webclient.entity.user.WebUser
 import trik.testsys.webclient.service.entity.user.WebUserService
 import trik.testsys.webclient.service.entity.user.WebUserService.Companion.isFirstTimeLoggedIn
 import trik.testsys.webclient.service.security.login.impl.LoginData
-import trik.testsys.webclient.util.addExitMessage
 import trik.testsys.webclient.util.addPopupMessage
-import trik.testsys.webclient.util.addSessionExpiredMessage
-import java.util.TimeZone
+import java.util.*
 
-/**
- * @author Roman Shishkin
- * @since 2.0.0
- **/
-abstract class WebUserController<U : WebUser, V : UserView<U>, S : WebUserService<U, out UserRepository<U>>>(
-    private val loginData: LoginData
-) {
-
-    protected abstract val MAIN_PATH: String
-
-    protected abstract val MAIN_PAGE: String
-
-    @Autowired
-    protected lateinit var service: S
+abstract class AbstractWebUserMainController<U : WebUser, V : UserView<U>, S : WebUserService<U, out UserRepository<U>>> (
+    loginData: LoginData
+) : AbstractWebUserController<U, V, S>(loginData) {
 
     @GetMapping
     open fun mainGet(
@@ -45,28 +31,28 @@ abstract class WebUserController<U : WebUser, V : UserView<U>, S : WebUserServic
             loginData.invalidate()
             return "redirect:${LoginController.LOGIN_PATH}"
         }
-        val webUser = validate(redirectAttributes) ?: return "redirect:${LoginController.LOGIN_PATH}"
+        val webUser = loginData.validate(redirectAttributes) ?: return "redirect:${LoginController.LOGIN_PATH}"
 
         model.addAttribute(WEB_USER_ATTR, webUser.toView(timeZone))
-        return MAIN_PAGE
+        return mainPage
     }
 
     @GetMapping(LOGIN_PATH)
     open fun loginGet(redirectAttributes: RedirectAttributes): String {
-        val webUser = validate(redirectAttributes) ?: return "redirect:${LoginController.LOGIN_PATH}"
+        val webUser = loginData.validate(redirectAttributes) ?: return "redirect:${LoginController.LOGIN_PATH}"
 
         if (webUser.isFirstTimeLoggedIn()) {
             redirectAttributes.addPopupMessage(
                 "Вы успешно зарегистрировались в системе! \n\n" +
-                "Пожалуйста, сохраните сгенерированный Код-доступа, чтобы не потерять его: \n\n" +
-                webUser.accessToken
+                        "Пожалуйста, сохраните сгенерированный Код-доступа, чтобы не потерять его: \n\n" +
+                        webUser.accessToken
             )
         }
 
         webUser.updateLastLoginDate()
         service.save(webUser)
 
-        return "redirect:$MAIN_PATH"
+        return "redirect:$mainPath"
     }
 
     @PostMapping(UPDATE_PATH)
@@ -75,59 +61,28 @@ abstract class WebUserController<U : WebUser, V : UserView<U>, S : WebUserServic
         timeZone: TimeZone,
         redirectAttributes: RedirectAttributes
     ): String {
-        validate(redirectAttributes) ?: return "redirect:${LoginController.LOGIN_PATH}"
+        loginData.validate(redirectAttributes) ?: return "redirect:${LoginController.LOGIN_PATH}"
 
         val updatedWebUser = webUserView.toEntity(timeZone)
 
         if (!service.validateName(updatedWebUser)) {
             redirectAttributes.addPopupMessage("Псевдоним не должен быть пустым, содержать Код–доступа или Код-регистрации. Попробуйте другой вариант.")
-            return "redirect:$MAIN_PATH"
+            return "redirect:$mainPath"
         }
         if (!service.validateAdditionalInfo(updatedWebUser)) {
             redirectAttributes.addPopupMessage("Дополнительная информация не должна содержать Код-доступа или Код-регистрации. Попробуйте другой вариант.")
-            return "redirect:$MAIN_PATH"
+            return "redirect:$mainPath"
         }
 
         service.save(updatedWebUser)
 
         redirectAttributes.addPopupMessage("Данные успешно изменены.")
-        return "redirect:$MAIN_PATH"
-    }
-
-    protected abstract fun U.toView(timeZone: TimeZone): V
-
-    /**
-     * @author Roman Shishkin
-     * @since 2.0.0
-     **/
-    protected fun validate(redirectAttributes: RedirectAttributes): U? {
-        val webUser = loginData.accessToken?.let { service.findByAccessToken(it) } ?: run {
-            loginData.invalidate()
-            redirectAttributes.addSessionExpiredMessage()
-            return null
-        }
-
-        return webUser
-    }
-
-    /**
-     * @author Roman Shishkin
-     * @since 2.0.0
-     **/
-    protected fun String?.checkLogout(redirectAttributes: RedirectAttributes): Boolean {
-        if (this != null) {
-            redirectAttributes.addExitMessage()
-            return true
-        }
-
-        return false
+        return "redirect:$mainPath"
     }
 
     companion object {
 
         const val LOGIN_PATH = "/login"
         const val UPDATE_PATH = "/update"
-
-        const val WEB_USER_ATTR = "webUser"
     }
 }
