6b7af7aaa92486dfd819e8e22ceda0f004e340d1
Roman Shishkin <st077017@student.spbu.ru>
2025-02-03 14:21:51 +0300

    Added judges creation

--- src/main/kotlin/trik/testsys/webclient/view/impl/SuperUserView.kt
+++ src/main/kotlin/trik/testsys/webclient/view/impl/SuperUserView.kt
@@ -4,6 +4,7 @@ import trik.testsys.core.entity.user.AccessToken
 import trik.testsys.core.view.user.UserView
 import trik.testsys.webclient.entity.impl.EmergencyMessage
 import trik.testsys.webclient.entity.user.impl.Developer
+import trik.testsys.webclient.entity.user.impl.Judge
 import trik.testsys.webclient.entity.user.impl.SuperUser
 import trik.testsys.webclient.entity.user.impl.Viewer
 import trik.testsys.webclient.util.fromTimeZone
@@ -22,6 +23,7 @@ data class SuperUserView(
     override val additionalInfo: String,
     val viewers: List<Viewer> = emptyList(),
     val developers: List<Developer> = emptyList(),
+    val judges: List<Judge> = emptyList(),
     val emergencyMessages: List<EmergencyMessage> = emptyList()
 ) : UserView<SuperUser> {
 
