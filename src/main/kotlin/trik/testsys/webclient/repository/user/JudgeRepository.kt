d31c8c407b55edda7cdcfce17110d675933bf658
Roman Shishkin <st077017@student.spbu.ru>
2025-08-18 23:23:55 +0500

    feat: Removed old classes

--- src/main/kotlin/trik/testsys/webclient/repository/user/JudgeRepository.kt
+++
@@ -1,8 +0,0 @@
-package trik.testsys.webclient.repository.user
-
-import org.springframework.stereotype.Repository
-import trik.testsys.core.repository.user.UserRepository
-import trik.testsys.webclient.entity.user.impl.Judge
-
-@Repository
-interface JudgeRepository : UserRepository<Judge>
