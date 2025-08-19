1194a4fa4b3f683bd188a0849b280db4ffcff1b3
Roman Shishkin <st077017@student.spbu.ru>
2025-08-20 02:58:39 +0500

    feat: Removed useless classes

--- src/main/kotlin/trik/testsys/webapp/backoffice/data/dto/EntityDto.kt
+++
@@ -1,9 +0,0 @@
-package trik.testsys.webapp.backoffice.data.dto
-
-import trik.testsys.webapp.core.data.entity.AbstractEntity
-
-/**
- * @author Roman Shishkin
- * @since %CURRENT_VERSION%
- */
-interface EntityDto<E : AbstractEntity>
