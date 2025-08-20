e2314737a353adfea11fa579fd154a404e863668
Roman Shishkin <st077017@student.spbu.ru>
2025-08-20 23:13:33 +0500

    feat: Commented TaskTemplate classes

--- src/main/kotlin/trik/testsys/webapp/backoffice/data/repository/TaskTemplateRepository.kt
+++ src/main/kotlin/trik/testsys/webapp/backoffice/data/repository/TaskTemplateRepository.kt
@@ -1,19 +1,19 @@
-package trik.testsys.webapp.backoffice.data.repository
-
-import org.springframework.stereotype.Repository
-import trik.testsys.webapp.backoffice.data.entity.impl.TaskTemplate
-import trik.testsys.webapp.backoffice.data.entity.impl.User
-import trik.testsys.webapp.backoffice.data.entity.impl.UserGroup
-import trik.testsys.webapp.core.data.repository.EntityRepository
-
-/**
- * @author Roman Shishkin
- * @since %CURRENT_VERSION%
- */
-@Repository
-interface TaskTemplateRepository : EntityRepository<TaskTemplate> {
-
-    fun findByDeveloper(developer: User): Set<TaskTemplate>
-
-    fun findDistinctByUserGroupsIn(userGroups: Collection<UserGroup>): Set<TaskTemplate>
-}
+//package trik.testsys.webapp.backoffice.data.repository
+//
+//import org.springframework.stereotype.Repository
+//import trik.testsys.webapp.backoffice.data.entity.impl.TaskTemplate
+//import trik.testsys.webapp.backoffice.data.entity.impl.User
+//import trik.testsys.webapp.backoffice.data.entity.impl.UserGroup
+//import trik.testsys.webapp.core.data.repository.EntityRepository
+//
+///**
+// * @author Roman Shishkin
+// * @since %CURRENT_VERSION%
+// */
+//@Repository
+//interface TaskTemplateRepository : EntityRepository<TaskTemplate> {
+//
+//    fun findByDeveloper(developer: User): Set<TaskTemplate>
+//
+//    fun findDistinctByUserGroupsIn(userGroups: Collection<UserGroup>): Set<TaskTemplate>
+//}
--- src/main/kotlin/trik/testsys/webapp/backoffice/data/service/impl/TaskTemplateServiceImpl.kt
+++ src/main/kotlin/trik/testsys/webapp/backoffice/data/service/impl/TaskTemplateServiceImpl.kt
@@ -1,27 +1,27 @@
-package trik.testsys.webapp.backoffice.data.service.impl
-
-import org.springframework.stereotype.Service
-import trik.testsys.webapp.backoffice.data.entity.impl.TaskTemplate
-import trik.testsys.webapp.backoffice.data.entity.impl.User
-import trik.testsys.webapp.backoffice.data.entity.impl.UserGroup
-import trik.testsys.webapp.backoffice.data.repository.TaskTemplateRepository
-import trik.testsys.webapp.backoffice.data.service.TaskTemplateService
-import trik.testsys.webapp.core.data.service.AbstractService
-
-/**
- * @author Roman Shishkin
- * @since %CURRENT_VERSION%
- */
-@Service
-class TaskTemplateServiceImpl :
-    AbstractService<TaskTemplate, TaskTemplateRepository>(),
-    TaskTemplateService {
-
-    override fun findByDeveloper(developer: User): Set<TaskTemplate> {
-        return repository.findByDeveloper(developer)
-    }
-
-    override fun findForUserGroups(userGroups: Collection<UserGroup>): Set<TaskTemplate> {
-        return repository.findDistinctByUserGroupsIn(userGroups)
-    }
-}
+//package trik.testsys.webapp.backoffice.data.service.impl
+//
+//import org.springframework.stereotype.Service
+//import trik.testsys.webapp.backoffice.data.entity.impl.TaskTemplate
+//import trik.testsys.webapp.backoffice.data.entity.impl.User
+//import trik.testsys.webapp.backoffice.data.entity.impl.UserGroup
+//import trik.testsys.webapp.backoffice.data.repository.TaskTemplateRepository
+//import trik.testsys.webapp.backoffice.data.service.TaskTemplateService
+//import trik.testsys.webapp.core.data.service.AbstractService
+//
+///**
+// * @author Roman Shishkin
+// * @since %CURRENT_VERSION%
+// */
+//@Service
+//class TaskTemplateServiceImpl :
+//    AbstractService<TaskTemplate, TaskTemplateRepository>(),
+//    TaskTemplateService {
+//
+//    override fun findByDeveloper(developer: User): Set<TaskTemplate> {
+//        return repository.findByDeveloper(developer)
+//    }
+//
+//    override fun findForUserGroups(userGroups: Collection<UserGroup>): Set<TaskTemplate> {
+//        return repository.findDistinctByUserGroupsIn(userGroups)
+//    }
+//}
