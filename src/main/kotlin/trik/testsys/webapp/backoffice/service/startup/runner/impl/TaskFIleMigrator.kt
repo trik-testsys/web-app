fa795b829095259eaa3c0ae66aa417fe878a5ecb
Roman Shishkin <st077017@student.spbu.ru>
2025-10-11 13:33:38 +0300

    feat: Updated migrator

--- src/main/kotlin/trik/testsys/webapp/backoffice/service/startup/runner/impl/TaskFIleMigrator.kt
+++ src/main/kotlin/trik/testsys/webapp/backoffice/service/startup/runner/impl/TaskFIleMigrator.kt
@@ -1,8 +1,11 @@
 package trik.testsys.webapp.backoffice.service.startup.runner.impl
 
+import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Propagation
 import org.springframework.transaction.annotation.Transactional
 import trik.testsys.webapp.backoffice.data.entity.AbstractFile
+import trik.testsys.webapp.backoffice.data.entity.impl.Solution
+import trik.testsys.webapp.backoffice.data.entity.impl.Task
 import trik.testsys.webapp.backoffice.data.entity.impl.TaskFile
 import trik.testsys.webapp.backoffice.data.entity.impl.taskFile.ConditionFile
 import trik.testsys.webapp.backoffice.data.entity.impl.taskFile.ExerciseFile
@@ -62,10 +65,12 @@ class TaskFIleMigrator(
                 var savedConditionFile = conditionFileService.save(conditionFile)
                 conditionFileMapping[taskFile.id!!] = savedConditionFile.id!!
 
-                taskFile.data.originalFileNameByVersion.keys.forEach { fileVersion ->
+                var fileVersion = taskFile.fileVersion
+                while (fileVersion >= 0) {
                     savedConditionFile.fileVersion++
                     val file = fileManager.getTaskFileVersion(taskFile, fileVersion)!!
                     savedConditionFile = fileManager.saveConditionFile(savedConditionFile, file)!!
+                    fileVersion--
                 }
 
                 logger.info("Successfully migrated condition file from TaskFile(id=${taskFile.id}) to ConditionFile(id=${savedConditionFile.id})")
@@ -82,10 +87,12 @@ class TaskFIleMigrator(
                 var savedExerciseFile = exerciseFileService.save(exerciseFile)
                 exerciseFileMapping[taskFile.id!!] = savedExerciseFile.id!!
 
-                taskFile.data.originalFileNameByVersion.keys.forEach { fileVersion ->
+                var fileVersion = taskFile.fileVersion
+                while (fileVersion >= 0) {
                     savedExerciseFile.fileVersion++
                     val file = fileManager.getTaskFileVersion(taskFile, fileVersion)!!
                     savedExerciseFile = fileManager.saveExerciseFile(savedExerciseFile, file)!!
+                    fileVersion--
                 }
 
                 logger.info("Successfully migrated exercise file from TaskFile(id=${taskFile.id}) to ExerciseFile(id=${savedExerciseFile.id})")
@@ -102,10 +109,12 @@ class TaskFIleMigrator(
                 var savedPolygonFile = polygonFileService.save(polygonFile)
                 polygonFileMapping[taskFile.id!!] = savedPolygonFile.id!!
 
-                taskFile.data.originalFileNameByVersion.keys.forEach { fileVersion ->
+                var fileVersion = taskFile.fileVersion
+                while (fileVersion >= 0) {
                     savedPolygonFile.fileVersion++
                     val file = fileManager.getTaskFileVersion(taskFile, fileVersion)!!
                     savedPolygonFile = fileManager.savePolygonFile(savedPolygonFile, file)!!
+                    fileVersion--
                 }
 
                 logger.info("Successfully migrated polygon file from TaskFile(id=${taskFile.id}) to PolygonFile(id=${savedPolygonFile.id})")
@@ -122,10 +131,12 @@ class TaskFIleMigrator(
                 var savedSolutionFile = solutionFileService.save(solutionFile)
                 solutionFileMapping[taskFile.id!!] = savedSolutionFile.id!!
 
-                taskFile.data.originalFileNameByVersion.keys.forEach { fileVersion ->
+                var fileVersion = taskFile.fileVersion
+                while (fileVersion >= 0) {
                     savedSolutionFile.fileVersion++
                     val file = fileManager.getTaskFileVersion(taskFile, fileVersion)!!
                     savedSolutionFile = fileManager.saveSolutionFile(savedSolutionFile, file)!!
+                    fileVersion--
                 }
 
                 logger.info("Successfully migrated solution file from TaskFile(id=${taskFile.id}) to SolutionFile(id=${savedSolutionFile.id})")
@@ -138,10 +149,10 @@ class TaskFIleMigrator(
         allTasks.forEach { task ->
             logger.debug("Migrating relations with task(id=${task.id})")
 
-            val conditions = task.taskFiles.filter { it.type == TaskFile.TaskFileType.CONDITION }
-            val exercises = task.taskFiles.filter { it.type == TaskFile.TaskFileType.EXERCISE }
-            val polygons = task.taskFiles.filter { it.type == TaskFile.TaskFileType.POLYGON }
-            val solutions = task.taskFiles.filter { it.type == TaskFile.TaskFileType.SOLUTION }
+            val conditions = task.taskFiles.filter { it.type == TaskFile.TaskFileType.CONDITION && !it.isRemoved }
+            val exercises = task.taskFiles.filter { it.type == TaskFile.TaskFileType.EXERCISE && !it.isRemoved }
+            val polygons = task.taskFiles.filter { it.type == TaskFile.TaskFileType.POLYGON && !it.isRemoved }
+            val solutions = task.taskFiles.filter { it.type == TaskFile.TaskFileType.SOLUTION && !it.isRemoved }
 
             logger.debug("Task containing conditions(size=${conditions.size}), exercises(size=${exercises.size}), polygons(size=${polygons.size}) and solutions(size=${solutions.size}).")
 
@@ -149,19 +160,41 @@ class TaskFIleMigrator(
             val exerciseFileIds = exercises.mapNotNull { exerciseFileMapping[it.id] }
             val polygonFileIds = polygons.mapNotNull { polygonFileMapping[it.id] }
 
-            val solitonFileScore = task.solutions.maxByOrNull { it.id!! }?.relevantVerdictId?.let {
+            val solution = task.solutions.filter { it.contest == null }.maxByOrNull { it.id!! }
+            val solitonFileScore = solution?.relevantVerdictId?.let {
                 val verdict = verdictService.findById(it)
                 verdict?.value
             } ?: 0L
-            val solutionFileIdsByScore = solutions.associate { solutionFileMapping[it.id]!! to solitonFileScore }
+            val solutionFileDataById = solutions.associate {
+                solutionFileMapping[it.id]!! to Task.SolutionFileData(
+                    Solution.SolutionType.QRS,
+                    solution?.id,
+                    solitonFileScore
+                )
+            }
 
             task.data.apply {
                 this.conditionFileIds.addAll(conditionFileIds)
                 this.exerciseFileIds.addAll(exerciseFileIds)
                 this.polygonFileIds.addAll(polygonFileIds)
-                this.solutionFileScoreById.putAll(solutionFileIdsByScore)
+                this.solutionFileDataById.putAll(solutionFileDataById)
             }
             taskService.save(task)
+
+            val conditionFiles = conditionFileService.findAllById(conditionFileIds)
+            val exerciseFiles = exerciseFileService.findAllById(exerciseFileIds)
+            val polygonFiles = polygonFileService.findAllById(polygonFileIds)
+            val solutionFiles = solutionFileService.findAllById(solutionFileDataById.keys)
+
+            conditionFiles.forEach { it.data.attachedTaskIds.add(task.id!!) }
+            exerciseFiles.forEach { it.data.attachedTaskIds.add(task.id!!) }
+            polygonFiles.forEach { it.data.attachedTaskIds.add(task.id!!) }
+            solutionFiles.forEach { it.data.attachedTaskIds.add(task.id!!) }
+
+            conditionFileService.saveAll(conditionFiles)
+            exerciseFileService.saveAll(exerciseFiles)
+            polygonFileService.saveAll(polygonFiles)
+            solutionFileService.saveAll(solutionFiles)
 
             logger.debug("Successfully migrated relations with task(id=${task.id}). New data: ${task.data}")
         }
